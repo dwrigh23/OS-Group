bfd784115ff323f01f97091784c205ce9b500bb9
asettle2 <asettle2@spsu.edu>
2015-03-04 17:18:34 -0500

    Update RAM.h

--- RAM.h
+++ RAM.h
@@ -3,7 +3,7 @@
 //OS Project
 #ifndef RAM_H 
 #define RAM_H
-#include <vector>
+#include <string>
 
 using namespace std;
 
@@ -11,9 +11,29 @@ class RAM {
 private:
 
 public:
-	vector<string> memory[1024];
-	int current;
-	int free;
+	array<string, 1024> memory;
+	int currentIndex = 0;
+	int currentFill = memory.size();
+	int maxFill = memory.max_size();
+
+	//Erases contents of RAM (the memory array)
+	void resetRam();
+
+	//Erases elements through specified start and ending locations
+	void freeRam(int startIndex, int endIndex);
+
+	//Populates the array
+	void writeRam(string instruction);
+
+	//Populates the array from a specified start position
+	void writeRamLoc(string instruction ,int startIndex);
+
+	//Returns amount of unpopulated locations in RAM
+	int getSpaceRemaining();
+
+	//Returns amounts of populated locations in RAM
+	int getCurrentFill();
 };
+RAM testRam;
 
 #endif
