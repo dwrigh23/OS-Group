9db83881dbfbef9aed3ba61a1a1125cf55a3fb87
asettle2 <asettle2@spsu.edu>
2015-03-03 19:02:16 -0500

    Update CPU.h

--- CPU.h
+++ CPU.h
@@ -1,8 +1,9 @@
 //Aaron Settle
-//3-1-2015
+//2-26-2015
 //OS Project
 #ifndef CPU_H 
 #define CPU_H
+#include "PCB.h"
 #include <string>
 
 using namespace std;
@@ -10,14 +11,22 @@ using namespace std;
 class CPU{
 private:
 	
+	
 public:
+	PCB thisProcess;
+
 	//array of 16 "registers"
 	int registers[16];
+	const int accumulator = 0;
+	const int zero_reg = 1;
+	int programCounter = 0;
 
 	//Retrieve an instruction from the disk
 	string fetch();
 
-	//Convert instructions to binary format (Base 2)
+	//Convert instructions to binary format (Base 16 --> Base 2)
+	//We decided not to include a method convert from bin to hex b/c there's no simple way to implement it
+	//and we are quickly running out of time.
 	string decode(string hex);
 	
 	//Hex to binary "Repository", if you will
