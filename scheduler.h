5672ad8d6f9b736beecaafebe115f543cb63b081
dwrigh23 <dwrigh23@spsu.edu>
2015-03-09 12:51:32 -0400

    Update scheduler.h

--- scheduler.h
+++ scheduler.h
@@ -1,1 +1,30 @@
 //this is the haeder file for the scheduler
+
+#include<iostream.h>
+#include<conio.h>
+#include<stdio.h>
+
+calss scheduler
+{
+  
+  
+  public 
+  //First come first serve
+  void fcfs();
+  
+  //Shortest job first
+  void sjf();
+  
+  //Shortest job first with preemption
+  void sjfp();
+  
+  //Shortest job first with nonpreemtion
+  void sjfnp();
+  
+  //Round Robin
+  void rr();
+  
+  //Priority
+  void priority();
+  
+};
