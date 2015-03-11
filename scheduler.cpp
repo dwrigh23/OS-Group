2b06c7b4086bc45999cfa9f22539515ed4640e1b
ethayer01 <thayer.everett@gmail.com>
2015-03-11 19:03:24 -0400

    Create scheduler.cpp

---
+++ scheduler.cpp
@@ -0,0 +1,17 @@
+//this file is for the scheduler
+//still working on if long and short will go together
+
+#include <string>
+#include <iostream>
+#include "Scheduler.h"
+
+using namespace std
+
+void scheduler::getdata(){
+  int i, n;
+  cout<<"Enter how many proceses you want to do.";
+  cin<<n;
+  for (i=1;i<=n;i++){
+    cout<<"Enter the burst time for the proceses."
+  }
+}
