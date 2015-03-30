dac0a96c0b859f68d792bf1807ce8b60382f2db8
asettle2 <asettle2@spsu.edu>
2015-03-30 18:07:31 -0400

    Create Quicksort.h

---
+++ Quicksort.h
@@ -0,0 +1,10 @@
+#ifndef QS_H
+#define QS_H
+#include <vector>
+
+using namespace std;
+
+int partition(vector<int>& test, int left, int right);
+
+void quickSort(vector<int>& test, int left, int right);
+#endif
