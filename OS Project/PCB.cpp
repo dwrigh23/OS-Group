bacc2f3a400ea49f2f922982fb9733282d09c7e3
asettle2 <asettle2@spsu.edu>
2015-04-01 16:22:46 -0400

    Accidentally deleted PCB.cpp

---
+++ OS Project/PCB.cpp
@@ -0,0 +1,16 @@
+#include "PCB.h"
+
+void PCB::createPCB(int jobID, int codeSize, int priority, int startDisk, int endDisk){
+	PCB pcb;
+
+	jobID = pcb.jobID;
+	codeSize = pcb.codeSize;
+	priority = pcb.priority;
+	startDisk = pcb.startDisk;
+	endDisk = pcb.endDisk;
+
+	pcb.programCounter = 0;
+	pcb.processState = pcb.created;
+
+	pcb.pcbVec.push_back(pcb);
+};
